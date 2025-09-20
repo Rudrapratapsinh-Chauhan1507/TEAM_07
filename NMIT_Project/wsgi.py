40c4b13fedfcf35aacb8117e1d942c9ad477edb6
yash-mandali <yashmandali211@gmail.com>
2025-09-20 10:34:59 +0530

    first setup

---
+++ NMIT_Project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NMIT_Project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NMIT_Project.settings')
+
+application = get_wsgi_application()
